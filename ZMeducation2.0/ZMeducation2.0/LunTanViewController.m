6264e1ee966c4ce6e68b0c363e793921a7df768a
licong <queen@QueendeiMac.local>
2017-07-21 17:18:48 +0800

    no message

--- ZMeducation2.0/ZMeducation2.0/LunTanViewController.m
+++ ZMeducation2.0/ZMeducation2.0/LunTanViewController.m
@@ -17,6 +17,14 @@
 
 @property (nonatomic, strong)UITableView *tabv;
 
+@property (nonatomic, strong)NSMutableArray *m007Arr;
+
+@property (nonatomic, strong)NSMutableDictionary *m007Dic;
+
+@property (nonatomic, weak)UIButton *selBtn;
+
+@property (nonatomic, weak)UILabel *selLb;
+
 @end
 
 @implementation LunTanViewController
@@ -54,14 +62,131 @@
     
     UIButton *selBtn = [[UIButton alloc]initWithFrame:CGRectMake(TitleLb.right + 15, 45, 280, 30)];
     [selBtn setImage:DEF_IMAGE(@"luntan_xuanze") forState:UIControlStateNormal];
-    [selBtn addTarget:selBtn action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];
+    [selBtn addTarget:self action:@selector(sel1:) forControlEvents:UIControlEventTouchUpInside];
     [bgImagv addSubview:selBtn];
+    self.selBtn = selBtn;
     
+    UILabel *selLb = [[UILabel alloc]initWithFrame:CGRectMake(10, 0, 280 - 30, 30)];
+    selLb.userInteractionEnabled = NO;
+    [selBtn addSubview:selLb];
+    self.selLb = selLb;
+    
+    self.tabv = [[UITableView alloc]initWithFrame:CGRectMake(selBtn.x, selBtn.bottom, selBtn.width, 300) style:UITableViewStylePlain];
+    self.tabv.delegate = self;
+    self.tabv.dataSource = self;
+    self.tabv.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tabv.hidden = YES;
+    [bgImagv addSubview:self.tabv];
+
+    UIButton *chaxunBtn = [[UIButton alloc]initWithFrame:CGRectMake(bgImagv.width - 130, 45, 90, 30)];
+    [chaxunBtn setImage:DEF_IMAGE(@"luntan_chaxun") forState:UIControlStateNormal];
+    [chaxunBtn addTarget:self action:@selector(chaxun) forControlEvents:UIControlEventTouchUpInside];
+    [bgImagv addSubview:chaxunBtn];
+    
+    
+    
+    NSDictionary * dic = @{@"version"          :@"2.0.0",
+                           @"clientType"       :@"1001",
+                           @"signType"         :@"md5",
+                           @"timestamp"        :[CACUtility getNowTime],
+                           @"method"           :@"M007",
+                           @"userId"           :self.userInfo[@"userId"],
+                           @"gradeId"          :self.userInfo[@"gradeId"],
+                           @"classId"          :self.userInfo[@"classId"],
+                           @"sign"             :[CACUtility getSignWithMethod:@"M007"]};
+    [RequestOperationManager getParametersDic:dic success:^(NSMutableDictionary *result) {
+        self.m007Arr = result[@"users"];
+        [self.tabv reloadData];
+    } failture:^(id result) {
+        
+    }];
+
 }
 
--(void)sel
+-(void)chaxun
 {
+    NSDictionary * dic = @{@"version"          :@"2.0.0",
+                           @"clientType"       :@"1001",
+                           @"signType"         :@"md5",
+                           @"timestamp"        :[CACUtility getNowTime],
+                           @"method"           :@"M032",
+                           @"userId"           :self.userInfo[@"userId"],
+                           @"gradeId"          :self.userInfo[@"gradeId"],
+                           @"classId"          :self.userInfo[@"classId"],
+                           @"courseId"         :self.userInfo[@"courseId"],
+                           @"unitId"           :self.dic[@"unitId"],
+                           @"unitTypeId"       :self.dic[@"unitTypeId"],
+                           @"authorId"         :self.m007Dic[@"userId"],
+                           @"sign"             :[CACUtility getSignWithMethod:@"M032"]};
+    [RequestOperationManager getParametersDic:dic success:^(NSMutableDictionary *result) {
+        
+    } failture:^(id result) {
+        
+    }];
+
+}
+
+-(void)sel1:(UIButton *)sender
+{
+    if (self.tabv.hidden == YES) {
+        self.tabv.hidden = NO;
+    }else{
+        self.tabv.hidden = YES;
+    }
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView *foot = [[UIView alloc] initWithFrame:CGRectMake(0, 0, DEF_DEVICE_WIDTH, 1)];
+    foot.alpha = 0.0f;
+    return foot;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    return 50;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.m007Arr count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
     
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.backgroundColor = [UIColor clearColor];
+        
+        UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, 49, self.tabv.width, 0.5)];
+        lineView.backgroundColor = [UIColor grayColor];
+        [cell.contentView addSubview:lineView];
+        
+    }
+    
+    cell.textLabel.text = self.m007Arr[indexPath.row][@"userName"];
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    self.m007Dic = self.m007Arr[indexPath.row];
+    self.tabv.hidden = YES;
+    self.selLb.text = self.m007Dic[@"userName"];
 }
 
 - (void)didReceiveMemoryWarning {
