e041a9b4375dad3688cc692de1e09f55fd1b1cbe
李聪 <827661334@qq.com>
2017-07-24 20:37:30 +0800

    no message

--- ZMeducation2.0/ZMeducation2.0/XiaoZuHeZuoViewController.m
+++ ZMeducation2.0/ZMeducation2.0/XiaoZuHeZuoViewController.m
@@ -9,7 +9,10 @@
 #import "XiaoZuHeZuoViewController.h"
 
 @interface XiaoZuHeZuoViewController ()
+
 @property (nonatomic, strong)NSMutableDictionary *userInfo;
+@property (nonatomic, weak) UIImageView *bgImagv;
+@property (nonatomic, strong)UISegmentedControl *seg;
 
 @end
 
@@ -28,6 +31,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.userInfo = [DEF_UserDefaults objectForKey:SAVE_USERINFO];
+}
+
+-(void)setDic:(NSDictionary *)dic
+{
+    _dic = dic;
+    
+    [self.bgImagv removeFromSuperview];
+    
+    UIImageView *bgImagv = [[UIImageView alloc]initWithFrame:CGRectMake(15, 15, DEF_DEVICE_WIDTH - 30 - 175, DEF_DEVICE_HEIGHT - 30 - 95)];
+    bgImagv.image = DEF_IMAGE(@"tiankongti_Bg");
+    bgImagv.userInteractionEnabled = YES;
+    [self.view addSubview:bgImagv];
+    self.bgImagv = bgImagv;
+
+    UIImageView *imageTitle = [[UIImageView alloc]initWithFrame:CGRectMake(0, 10, 80, 30)];
+    imageTitle.contentMode = UIViewContentModeScaleAspectFit;
+    imageTitle.image = DEF_IMAGE(@"xiaozuhezuo_title");
+    imageTitle.centerX = bgImagv.centerX;
+    [bgImagv addSubview:imageTitle];
+
+    NSArray *arr = @[@"合作内容填写",@"合作小组浏览",@"合作分项浏览",@"合作文稿生成"];
+    self.seg = [[UISegmentedControl alloc]initWithItems:arr];
+    self.seg.frame = CGRectMake(35, 65, 500, 35);
+    self.seg.selectedSegmentIndex = 0;
+    self.seg.tintColor = DEF_COLOR_RGB(0, 154, 221);
+
+    [bgImagv addSubview:self.seg];
+    [self.seg addTarget:self action:@selector(segmentAction:) forControlEvents:UIControlEventValueChanged];
+
+}
+
+-(void)segmentAction:(UISegmentedControl *)Seg
+{
+    
 }
 
 - (void)didReceiveMemoryWarning {
